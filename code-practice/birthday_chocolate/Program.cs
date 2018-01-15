802bddeb1e69e2b06e88641137f837712232e39f
Aparanta <aparanta53@gmail.com>
2018-01-15 11:10:05 +0530

    hackerrank

---
+++ code-practice/birthday_chocolate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace birthday_chocolate
+{
+
+    class Program
+    {
+       static int twoCharaters(string s) {
+        // Complete this function
+    }
+
+    static void Main(String[] args) {
+        int l = Convert.ToInt32(Console.ReadLine());
+        string s = Console.ReadLine();
+        int result = twoCharaters(s);
+        Console.WriteLine(result);
+    }
+
+
+    }
+}
